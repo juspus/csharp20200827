1d0224cf6d334b380e723d0f1a018903e99ac47b
Juspus <juspus@users.noreply.github.com>
2020-09-17 21:28:20 +0300

    Skaiciavimo masinele

---
+++ _20200917_SkaiciavimoMasinele/_20200917_SkaiciavimoMasinele/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace _20200917_SkaiciavimoMasinele
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.eqBtn = new System.Windows.Forms.Button();
+            this.CEBtn = new System.Windows.Forms.Button();
+            this.dalBtn = new System.Windows.Forms.Button();
+            this.daugBtn = new System.Windows.Forms.Button();
+            this.minBtn = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.IvedimoTxtBx = new System.Windows.Forms.TextBox();
+            this.IsvedimoTxtBx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(149, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 62);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(78, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 62);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(149, 164);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 62);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 96);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 62);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(78, 96);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 62);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(149, 96);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(65, 62);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(78, 300);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(65, 62);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // eqBtn
+            // 
+            this.eqBtn.Location = new System.Drawing.Point(7, 300);
+            this.eqBtn.Name = "eqBtn";
+            this.eqBtn.Size = new System.Drawing.Size(65, 62);
+            this.eqBtn.TabIndex = 10;
+            this.eqBtn.Text = "=";
+            this.eqBtn.UseVisualStyleBackColor = true;
+            this.eqBtn.Click += new System.EventHandler(this.eqBtn_Click);
+            // 
+            // CEBtn
+            // 
+            this.CEBtn.Location = new System.Drawing.Point(149, 300);
+            this.CEBtn.Name = "CEBtn";
+            this.CEBtn.Size = new System.Drawing.Size(65, 62);
+            this.CEBtn.TabIndex = 11;
+            this.CEBtn.Text = "CE";
+            this.CEBtn.UseVisualStyleBackColor = true;
+            this.CEBtn.Click += new System.EventHandler(this.CEBtn_Click);
+            // 
+            // dalBtn
+            // 
+            this.dalBtn.Location = new System.Drawing.Point(220, 96);
+            this.dalBtn.Name = "dalBtn";
+            this.dalBtn.Size = new System.Drawing.Size(65, 62);
+            this.dalBtn.TabIndex = 12;
+            this.dalBtn.Text = "/";
+            this.dalBtn.UseVisualStyleBackColor = true;
+            this.dalBtn.Click += new System.EventHandler(this.dalBtn_Click);
+            // 
+            // daugBtn
+            // 
+            this.daugBtn.Location = new System.Drawing.Point(220, 164);
+            this.daugBtn.Name = "daugBtn";
+            this.daugBtn.Size = new System.Drawing.Size(65, 62);
+            this.daugBtn.TabIndex = 13;
+            this.daugBtn.Text = "*";
+            this.daugBtn.UseVisualStyleBackColor = true;
+            this.daugBtn.Click += new System.EventHandler(this.daugBtn_Click);
+            // 
+            // minBtn
+            // 
+            this.minBtn.Location = new System.Drawing.Point(220, 232);
+            this.minBtn.Name = "minBtn";
+            this.minBtn.Size = new System.Drawing.Size(65, 62);
+            this.minBtn.TabIndex = 14;
+            this.minBtn.Text = "-";
+            this.minBtn.UseVisualStyleBackColor = true;
+            this.minBtn.Click += new System.EventHandler(this.minBtn_Click);
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Location = new System.Drawing.Point(220, 300);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(65, 62);
+            this.plusBtn.TabIndex = 15;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // IvedimoTxtBx
+            // 
+            this.IvedimoTxtBx.AcceptsTab = true;
+            this.IvedimoTxtBx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.IvedimoTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.IvedimoTxtBx.Location = new System.Drawing.Point(7, 52);
+            this.IvedimoTxtBx.Name = "IvedimoTxtBx";
+            this.IvedimoTxtBx.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.IvedimoTxtBx.Size = new System.Drawing.Size(278, 31);
+            this.IvedimoTxtBx.TabIndex = 16;
+            this.IvedimoTxtBx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // IsvedimoTxtBx
+            // 
+            this.IsvedimoTxtBx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.IsvedimoTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.IsvedimoTxtBx.Location = new System.Drawing.Point(7, 8);
+            this.IsvedimoTxtBx.Name = "IsvedimoTxtBx";
+            this.IsvedimoTxtBx.ReadOnly = true;
+            this.IsvedimoTxtBx.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.IsvedimoTxtBx.Size = new System.Drawing.Size(278, 31);
+            this.IsvedimoTxtBx.TabIndex = 17;
+            this.IsvedimoTxtBx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 377);
+            this.Controls.Add(this.IsvedimoTxtBx);
+            this.Controls.Add(this.IvedimoTxtBx);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.minBtn);
+            this.Controls.Add(this.daugBtn);
+            this.Controls.Add(this.dalBtn);
+            this.Controls.Add(this.CEBtn);
+            this.Controls.Add(this.eqBtn);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button eqBtn;
+        private System.Windows.Forms.Button CEBtn;
+        private System.Windows.Forms.Button dalBtn;
+        private System.Windows.Forms.Button daugBtn;
+        private System.Windows.Forms.Button minBtn;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.TextBox IvedimoTxtBx;
+        private System.Windows.Forms.TextBox IsvedimoTxtBx;
+    }
+}
+
---
+++ _20200917_SkaiciavimoMasinele/_20200917_SkaiciavimoMasinele/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20200917_SkaiciavimoMasinele
+{    
+    public partial class Form1 : Form
+    {
+        private char veiksmas { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            veiksmas = ' ';
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var c = e.KeyChar;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "9";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text += "3";
+        }
+
+        private void eqBtn_Click(object sender, EventArgs e)
+        {
+            VeiksmoTikrinimas();
+            veiksmas = ' ';
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if(IvedimoTxtBx.Text.Length == 0)
+            {
+                IvedimoTxtBx.Text += "0.";
+            }
+            else
+            {
+                IvedimoTxtBx.Text += "0";
+            }            
+        }
+
+        private void CEBtn_Click(object sender, EventArgs e)
+        {
+            IvedimoTxtBx.Text = "";
+            IsvedimoTxtBx.Text = "";
+        }
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            VeiksmoTikrinimas();
+            veiksmas = '+';
+        }
+
+        private void minBtn_Click(object sender, EventArgs e)
+        {
+            VeiksmoTikrinimas();
+            veiksmas = '-';
+        }
+
+        private void daugBtn_Click(object sender, EventArgs e)
+        {
+            VeiksmoTikrinimas();
+            veiksmas = '*';
+        }
+
+        private void dalBtn_Click(object sender, EventArgs e)
+        {
+            VeiksmoTikrinimas();
+            veiksmas = '/';
+        }
+
+        private void VeiksmoTikrinimas()
+        {
+            if (IsvedimoTxtBx.Text.Length == 0)
+            {
+                IsvedimoTxtBx.Text = IvedimoTxtBx.Text;
+            }
+            else if (!char.IsWhiteSpace(veiksmas))
+            {
+                IsvedimoTxtBx.Text = MatematinisVeikmas().ToString();
+            }
+            IvedimoTxtBx.Text = "";
+        }
+
+        private double MatematinisVeikmas()
+        {
+            if(veiksmas == '+')
+            {
+                return double.Parse(IsvedimoTxtBx.Text) + double.Parse(IvedimoTxtBx.Text);
+            }
+            else if (veiksmas == '-')
+            {
+                return double.Parse(IsvedimoTxtBx.Text) - double.Parse(IvedimoTxtBx.Text);
+
+            }
+            else if (veiksmas == '*')
+            {
+                return double.Parse(IsvedimoTxtBx.Text) * double.Parse(IvedimoTxtBx.Text);
+
+            }
+            else if (veiksmas == '/')
+            {
+                if(double.Parse(IvedimoTxtBx.Text) == 0)
+                {
+                    MessageBox.Show("Dalyba is nulio negalima");
+                    return 0;
+                }
+                return double.Parse(IsvedimoTxtBx.Text) / double.Parse(IvedimoTxtBx.Text);
+            }
+            return 0;
+        }
+    }
+}
