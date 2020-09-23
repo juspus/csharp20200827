a030e611a4fd9635a0dc9ea68a6392564b75f37d
Juspus <juspus@users.noreply.github.com>
2020-09-23 14:32:11 +0300

    prideti komentarai

--- _20200921/_20200921/Program.cs
+++ _20200921/_20200921/Program.cs
@@ -12,10 +12,29 @@ namespace _20200921
         private static Random rng;
         static void Main(string[] args)
         {
+
+            /*Aprašymas:
+             * Susikūrėme Spalvos klasę su 4 spalvos kanalais
+             * Susikūrėme Kamulio klasę, kuris turėjo spindulį, sprogimo indikatorių, firmos pavadinimą ir spalvą, kuri buvo mūsų spalvos klasės objektas
+             * Kamuolio klasė turi virtualų sprogimo garso metodą, kuris į consolę atspausdina sprogimo garsą.
+             * Kamuolio klasė turi sprogdinimo metodą, kurį iškviečiame kamuoliui sprogstant - jis pakeičia spindulį ir sprogimo indikatorių.
+             * Kamuolio klasė turi virtualų metimo metodą kuris pagal atsitiktinių skaičių generatorių bando susprogdinti kamuolį naudodamas sprogdinti metodą ir jei kamuolys nesprogęs jį meta prideda prie metimu skaičiaus atributo 1.
+             * Kamuolio klasė turi virtualų informacijos metodą, kuris atspausdina spindulį, spalvą, firmą ir metimų skaičių.
+             * Susikūrėme 3 skirtingas klases, kurios paveldi kamuolio bazinę klasę: Tinklinio, Teniso, Krepsinio Kamuoliai.
+             * Kiekviena iš jų gavo papildomą atributą ,kuris nurodė kamuolio tipą.
+             * Taip pat jose buvo perrašomas sprogimo garso metodas (override) ir nurodomas skirtingas sprogimo garsas, kurį išleidžia sprogstantis kamuolys.
+             * Su override perrašomi informacijos metodas kiekienoje klasėje, kuris papildomai atspausdina ir kamuolio atitinkamą kamuolio tipą.
+             * 
+             * Boulingo kamuolys yra išskirtinis, kad jame pridėjome daugiau atributų, bei perrašėme (override) mesti metodą, kad kamuolys sprogtų tik po galiojimo pabaigos.
+             * 
+             * Pagrindinėje programoje susikūrėme pagalbinių metodų ir mėteme kamuolius tol, kol visi jie sprogo. Atspasudindami informaciją prieš metimus, po metimų ir sprogimo garsą.
+             * 
+             * */
             rng = new Random();
             int kamuoliuKiekis = rng.Next(20, 100);
             List<Kamuolys> pintine = UzpildauRusiKamuoliais(kamuoliuKiekis);
 
+            //Kamuolių mėtymo ciklas
             foreach(Kamuolys ball in pintine)
             {
                 ball.Info();
@@ -30,6 +49,11 @@ namespace _20200921
             Console.Read();
         }
 
+        /// <summary>
+        /// Metodas, kuris mums sugeneruoja tam tikrą kiekį atsitiktinių kamulių, juos sudeda į sąrašą
+        /// </summary>
+        /// <param name="kiekis">Atsitiktinis kamuolių kiekis, kurį norime sugeneruoti.</param>
+        /// <returns>Suvestą kamuolių sąrašą</returns>
         public static List<Kamuolys> UzpildauRusiKamuoliais(int kiekis)
         {
             List<Kamuolys> rusys = new List<Kamuolys>();
@@ -39,12 +63,19 @@ namespace _20200921
             }
             return rusys;
         }
-
+        /// <summary>
+        /// Iš masyvo su firmų pavadinimais atsitiktinai parenkame vieną iš firmos pavadinimų.
+        /// </summary>
+        /// <param name="firmos">Masyvas su įrašytomis žinomomis firmomis.</param>
+        /// <returns></returns>
         public static string FirmosPavadinimoParinkimas(string[] firmos)
         {            
             return firmos[rng.Next(0, firmos.Length)];
         }
-
+        /// <summary>
+        /// Atsitiktinai parenkamas kamuolio tipas ir jis sugeneruojamas kaip paprastas kamuolys.
+        /// </summary>
+        /// <returns>Atsitiktinio tipo kamuolį. Nes visi tie tipai yra kamuoliai, tik kitokie.</returns>
         public static Kamuolys KamuolioGeneravimas()
         {
             int rngSk = rng.Next(0, 3);
